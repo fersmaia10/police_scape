9474af3a840cd9c3549bdbab198090ee748dabb2
fersmaia10 <fersmaia@icloud.com>
2025-03-15 01:46:11 -0300

    Adicionando a janela com o background do menu

--- code/Menu.py
+++ code/Menu.py
@@ -1,6 +1,22 @@
+<<<<<<< HEAD
 class Menu:
     def __init__(self, window):
         self.window = window
 
         def run(self,):
             pass
+=======
+import pygame.image
+
+
+class Menu:
+    def __init__(self, window):
+        self.window = window
+        self.surf = pygame.image.load('./asset/menuBG.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
+
+    def run(self, ):
+        self.window.blit(source=self.surf, dest=self.rect)
+        pygame.display.flip()
+        pass
+>>>>>>> 8ee638a (Adicionando a janela com o background do menu)
--- main.py
+++ main.py
@@ -2,3 +2,7 @@ from code.Game import Game
 
 game = Game()
 game.run()
+<<<<<<< HEAD
+=======
+
+>>>>>>> 8ee638a (Adicionando a janela com o background do menu)
